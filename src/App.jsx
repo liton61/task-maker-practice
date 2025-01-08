9415dc1c6936a5489e359e704384cf9ac0def819
liton61 <litonofficial61@gmail.com>
2025-01-08 23:34:42 +0600

    task board added

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import Footer from "./components/Footer";
 import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
+import TaskBoard from "./components/TaskBoard";
 
 function App() {
   return (
     <>
       <Navbar />
-      <Hero />
+      <div className="flex flex-col items-center justify-center">
+        <Hero />
+        <TaskBoard />
+      </div>
       <Footer />
     </>
   );
--- src/components/TaskBoard.jsx
+++ src/components/TaskBoard.jsx
@@ -1,11 +1,21 @@
-
+import SearchBar from "./SearchBar";
+import TaskAction from "./TaskAction";
+import TaskList from "./TaskList";
 
 const TaskBoard = () => {
-    return (
-        <div>
-            
+  return (
+    <>
+      <section className="mb-20" id="tasks">
+        <div className="container">
+          <SearchBar />
+          <div className="rounded-xl border border-[rgba(206,206,206,0.12)] bg-[#1D212B] px-6 py-8 md:px-9 md:py-16">
+            <TaskAction />
+            <TaskList />
+          </div>
         </div>
-    );
+      </section>
+    </>
+  );
 };
 
 export default TaskBoard;
